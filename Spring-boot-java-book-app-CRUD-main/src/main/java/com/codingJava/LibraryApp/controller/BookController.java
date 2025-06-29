6bb440163bca2c962aec48021e2676a4e24709d8
barskarlalit <56199222+barskarlalit@users.noreply.github.com>
2025-06-29 20:44:29 +0530

    Update BookController.java
    new changes

--- Spring-boot-java-book-app-CRUD-main/src/main/java/com/codingJava/LibraryApp/controller/BookController.java
+++ Spring-boot-java-book-app-CRUD-main/src/main/java/com/codingJava/LibraryApp/controller/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//RestControler
 @RestController
 @RequestMapping("/book/v1")
 public class BookController {
